72721b5a0dded0a1eb3069d9cd10b67c3d569c9b
Ahmed Al-Hindawi <a.al-hindawi@imperial.ac.uk>
2022-02-16 02:40:41 +0000

    fixed __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -1,4 +1,4 @@
 from .GazeVGG import *
 from .GazeResNetMLP import *
 from .GazeDecoder import ResNet18Dec
-from .GazeResNetEncoder import GazeEncoder, GazeDecoder
+from .GazeResNetEncoder import GazeEncoder
