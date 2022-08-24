7bbe1452a78e9c7ddec6b0cdc91677aa5ab691bb
Ahmed Al-Hindawi <a.al-hindawi@imperial.ac.uk>
2022-08-24 16:10:45 +0100

    combined dataset from folder and from hdf5 into the trainer so that both can be used

--- datasets/RTGENEDataset.py
+++ datasets/RTGENEDataset.py
@@ -5,14 +5,14 @@ from torch.utils import data
 from torchvision import transforms
 from tqdm import tqdm
 from PIL import Image
+import h5py
 
 
 class RTGENEFileDataset(data.Dataset):
 
-    def __init__(self, root_path, subject_list=None, eye_transform=None, face_transform=None):
+    def __init__(self, root_path, subject_list=None, eye_transform=None):
         self._root_path = root_path
         self._eye_transform = eye_transform
-        self._face_transform = face_transform
         self._subject_labels = []
 
         assert subject_list is not None, "Must pass a list of subjects to load the data for"
@@ -21,11 +21,6 @@ class RTGENEFileDataset(data.Dataset):
             self._eye_transform = transforms.Compose([transforms.Resize((36, 60), transforms.InterpolationMode.NEAREST),
                                                       transforms.ToTensor(),
                                                       transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
-
-        if self._face_transform is None:
-            self._face_transform = transforms.Compose([transforms.Resize((224, 224), transforms.InterpolationMode.NEAREST),
-                                                       transforms.ToTensor(),
-                                                       transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
         subject_path = [os.path.join(root_path, "s{:03d}_glasses/".format(_i)) for _i in subject_list]
 
@@ -54,12 +49,57 @@ class RTGENEFileDataset(data.Dataset):
 
         left_img = Image.open(os.path.join(self._root_path, sample[0])).convert('RGB')
         right_img = Image.open(os.path.join(self._root_path, sample[1])).convert('RGB')
-        face_img = Image.open(os.path.join(self._root_path, sample[2])).convert('RGB')
 
         transformed_left = self._eye_transform(left_img)
         transformed_right = self._eye_transform(right_img)
-        transformed_face = self._face_transform(face_img)
-        #
-        return np.asarray(left_img), np.asarray(right_img), np.asarray(face_img), \
-               transformed_left, transformed_right, transformed_face, \
-               np.array(ground_truth_headpose, dtype=np.float32), np.array(ground_truth_gaze, dtype=np.float32)
+
+        return np.asarray(left_img), np.asarray(right_img), transformed_left, transformed_right, np.array(ground_truth_headpose,
+                                                                                                          dtype=np.float32), np.array(
+            ground_truth_gaze, dtype=np.float32)
+
+
+class RTGENEH5Dataset(data.Dataset):
+
+    def __init__(self, root_path, subject_list=None, eye_transform=None):
+        self._h5_file = root_path
+        self._transform = eye_transform
+        self._subject_labels = []
+
+        assert subject_list is not None, "Must pass a list of subjects to load the data for"
+
+        if self._transform is None:
+            self._transform = transforms.Compose([transforms.Resize((36, 60)),
+                                                  transforms.ToTensor(),
+                                                  transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+
+        wanted_subjects = ["s{:03d}".format(_i) for _i in subject_list]
+
+        with h5py.File(self._h5_file, mode="r") as dataset:
+            for grp_s_n in tqdm(wanted_subjects, desc="Loading subject metadata..."):  # subjects
+                for grp_i_n, grp_i in dataset[grp_s_n].items():  # images
+                    if "left" in grp_i.keys() and "right" in grp_i.keys() and "label" in grp_i.keys():
+                        left_dataset = grp_i["left"]
+                        right_datset = grp_i['right']
+
+                        assert len(left_dataset) == len(right_datset), "Dataset left/right images aren't equal length"
+                        self._subject_labels.append("/" + grp_s_n + "/" + grp_i_n)
+
+    def __len__(self):
+        return len(self._subject_labels)
+
+    def __getitem__(self, index):
+        sample = self._subject_labels[index]
+
+        with h5py.File(self._h5_file, mode="r") as dataset:
+            left_img = dataset[sample + "/left"][0][()]
+            right_img = dataset[sample + "/right"][0][()]
+            label_data = dataset[sample + "/label"][()]
+
+        ground_truth_headpose = label_data[0][()].astype(np.float32)
+        ground_truth_gaze = label_data[1][()].astype(np.float32)
+
+        # Load data and get label
+        transformed_left = self._transform(Image.fromarray(left_img, 'RGB'))
+        transformed_right = self._transform(Image.fromarray(right_img, 'RGB'))
+
+        return left_img, right_img, transformed_left, transformed_right, ground_truth_headpose, ground_truth_gaze
--- training/train_model.py
+++ training/train_model.py
@@ -6,22 +6,23 @@ from functools import partial
 import h5py
 import pytorch_lightning as pl
 import torch
-from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
+from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor, StochasticWeightAveraging
 from torch.utils.data import DataLoader
 from torchmetrics import MetricCollection
 from torchvision.transforms import transforms
 
-from gaze_estimation.datasets.RTGENEDataset import RTGENEFileDataset as RTGENEDataset
+from gaze_estimation.datasets.RTGENEDataset import RTGENEFileDataset, RTGENEH5Dataset
 from gaze_estimation.model import GazeEstimationModelVGG, GazeEstimationModelResNet
 from gaze_estimation.training.LAMB import LAMB
 from gaze_estimation.training.utils import GazeAngleAccuracyMetric
+
 
 class GNLL(torch.nn.Module):
     def __init__(self):
         super(GNLL, self).__init__()
 
     def forward(self, angle_out, label, var):
-        loss = 0.5 * (torch.mul(torch.exp(-var), (angle_out - label) ** 2) + var)
+        loss = 0.5 * ((torch.exp(-var) * (angle_out - label) ** 2) + var)
         return loss.mean()
 
 
@@ -44,10 +45,11 @@ OPTIMISERS = {
 
 class TrainRTGENE(pl.LightningModule):
 
-    def __init__(self, hparams, train_subjects, validate_subjects, test_subjects):
+    def __init__(self, hparams, train_subjects, validate_subjects, test_subjects, dataset):
         super(TrainRTGENE, self).__init__()
 
         self.model = MODELS[hparams.model_base](num_out=LOSS_FN[hparams.loss_fn]["num_out"])
+        self._dataset = dataset
         self._criterion = LOSS_FN[hparams.loss_fn]["loss"]()
         self._metrics = MetricCollection([GazeAngleAccuracyMetric(reduction="mean")])
         self._train_subjects = train_subjects
@@ -59,7 +61,7 @@ class TrainRTGENE(pl.LightningModule):
         return self.model(left_patch, right_patch, head_pose)
 
     def shared_step(self, batch):
-        _, _, _, left_patch, right_patch, _, headpose_label, gaze_labels = batch
+        _, _, left_patch, right_patch, headpose_label, gaze_labels = batch
 
         y, _ = self.forward(left_patch, right_patch, headpose_label)
         angle_out = y[:, :2]
@@ -106,64 +108,53 @@ class TrainRTGENE(pl.LightningModule):
                                             transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                                                  std=[0.229, 0.224, 0.225])])
 
-        face_transform = transforms.Compose([transforms.ToTensor(),
-                                             transforms.RandomResizedCrop(size=(36, 60), scale=(0.5, 1.3)),
-                                             transforms.RandomGrayscale(p=0.1),
-                                             transforms.ColorJitter(brightness=0.5, hue=0.2, contrast=0.5, saturation=0.5),
-                                             transforms.RandomApply([transforms.GaussianBlur(3, sigma=(0.1, 2.0))], p=0.1),
-                                             transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                                                  std=[0.229, 0.224, 0.225])])
-
-        data = RTGENEDataset(root_path=self.hparams.dataset_path, subject_list=self._train_subjects, eye_transform=eye_transform, face_transform=face_transform)
+        data = self._dataset(subject_list=self._train_subjects, eye_transform=eye_transform)
+        print(len(data))
         return DataLoader(data, batch_size=self.hparams.batch_size, shuffle=True, num_workers=self.hparams.num_io_workers, pin_memory=True)
 
     def val_dataloader(self):
-        eye_transform = transforms.Compose([transforms.ToTensor(),
-                                            transforms.RandomResizedCrop(size=(36, 60), scale=(0.5, 1.3)),
-                                            transforms.RandomGrayscale(p=0.1),
-                                            transforms.ColorJitter(brightness=0.5, hue=0.2, contrast=0.5, saturation=0.5),
-                                            transforms.RandomApply([transforms.GaussianBlur(3, sigma=(0.1, 2.0))], p=0.1),
-                                            transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                                                 std=[0.229, 0.224, 0.225])])
-
-        face_transform = transforms.Compose([transforms.ToTensor(),
-                                             transforms.RandomResizedCrop(size=(36, 60), scale=(0.5, 1.3)),
-                                             transforms.RandomGrayscale(p=0.1),
-                                             transforms.ColorJitter(brightness=0.5, hue=0.2, contrast=0.5, saturation=0.5),
-                                             transforms.RandomApply([transforms.GaussianBlur(3, sigma=(0.1, 2.0))], p=0.1),
-                                             transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                                                  std=[0.229, 0.224, 0.225])])
-
-        data = RTGENEDataset(root_path=self.hparams.dataset_path, subject_list=self._validate_subjects, eye_transform=eye_transform, face_transform=face_transform)
+        data = self._dataset(subject_list=self._validate_subjects, eye_transform=None)
         return DataLoader(data, batch_size=self.hparams.batch_size, shuffle=False, num_workers=self.hparams.num_io_workers, pin_memory=True)
 
     def test_dataloader(self):
-        eye_transform = transforms.Compose([transforms.ToTensor(),
-                                            transforms.RandomResizedCrop(size=(36, 60), scale=(0.5, 1.3)),
-                                            transforms.RandomGrayscale(p=0.1),
-                                            transforms.ColorJitter(brightness=0.5, hue=0.2, contrast=0.5, saturation=0.5),
-                                            transforms.RandomApply([transforms.GaussianBlur(3, sigma=(0.1, 2.0))], p=0.1),
-                                            transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                                                 std=[0.229, 0.224, 0.225])])
-
-        face_transform = transforms.Compose([transforms.ToTensor(),
-                                             transforms.RandomResizedCrop(size=(36, 60), scale=(0.5, 1.3)),
-                                             transforms.RandomGrayscale(p=0.1),
-                                             transforms.ColorJitter(brightness=0.5, hue=0.2, contrast=0.5, saturation=0.5),
-                                             transforms.RandomApply([transforms.GaussianBlur(3, sigma=(0.1, 2.0))], p=0.1),
-                                             transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                                                  std=[0.229, 0.224, 0.225])])
-
-        data = RTGENEDataset(root_path=self.hparams.dataset_path, subject_list=self._test_subjects, eye_transform=eye_transform, face_transform=face_transform)
-        return DataLoader(data, batch_size=self.hparams.batch_size, shuffle=True, num_workers=self.hparams.num_io_workers, pin_memory=True)
+        data = self._dataset(subject_list=self._test_subjects, eye_transform=None)
+        return DataLoader(data, batch_size=self.hparams.batch_size, shuffle=False, num_workers=self.hparams.num_io_workers, pin_memory=True)
 
     def configure_optimizers(self):
-        def linear_warmup_decay(warmup_steps):
+        # def linear_warmup_decay(warmup_steps):
+        #     def fn(step):
+        #         if step < warmup_steps:
+        #             return float(step) / float(max(1, warmup_steps))
+        #
+        #         return 1.0
+        #
+        #     return fn
+
+        def linear_warmup_decay(warmup_steps, total_steps, cosine=True, linear=False):
+            """
+            Linear warmup for warmup_steps, optionally with cosine annealing or
+            linear decay to 0 at total_steps
+            """
+            # check if both decays are not True at the same time
+            assert not (linear and cosine)
+
             def fn(step):
                 if step < warmup_steps:
                     return float(step) / float(max(1, warmup_steps))
 
-                return 1.0
+                if not (cosine or linear):
+                    # no decay
+                    return 1.0
+
+                progress = float(step - warmup_steps) / float(
+                    max(1, total_steps - warmup_steps)
+                )
+                if cosine:
+                    # cosine decay
+                    return 0.5 * (1.0 + math.cos(math.pi * progress))
+
+                # linear decay
+                return 1.0 - progress
 
             return fn
 
@@ -175,10 +166,11 @@ class TrainRTGENE(pl.LightningModule):
         optimizer = OPTIMISERS.get(self.hparams.optimiser)(params_to_update, lr=self.hparams.learning_rate, weight_decay=self.hparams.weight_decay)
 
         if self.hparams.optimiser_schedule:
-            warmup_steps = (81152 / self.hparams.batch_size) * self.hparams.warmup_epochs
+            warmup_steps = (74804 / self.hparams.batch_size) * self.hparams.warmup_epochs  # 74804 is from len(training_dataset)
+            total_steps = (74804 / self.hparams.batch_size) * self.hparams.max_epochs
 
             scheduler = {
-                "scheduler": torch.optim.lr_scheduler.LambdaLR(optimizer, linear_warmup_decay(warmup_steps)),
+                "scheduler": torch.optim.lr_scheduler.LambdaLR(optimizer, linear_warmup_decay(warmup_steps, total_steps=total_steps)),
                 "interval": "step",
                 "frequency": 1,
             }
@@ -210,15 +202,22 @@ if __name__ == "__main__":
 
     root_parser = ArgumentParser(add_help=False)
     root_parser.add_argument('--gpu', type=int, default=-1)
+    root_parser.add_argument('--dataset_type', nargs='?', choices=('hdf5', 'folder'))
     root_parser.add_argument('--dataset_path', type=str, required=True)
-    root_parser.add_argument('--dataset_type', type=str, choices=["rt_gene", "other"], default="rt_gene")
+    root_parser.add_argument('--dataset_name', type=str, choices=["rt_gene", "other"], default="rt_gene")
     root_parser.add_argument('--num_io_workers', default=psutil.cpu_count(logical=False), type=int)
     root_parser.add_argument('--k_fold_validation', action="store_true", dest="k_fold_validation")
     root_parser.add_argument('--all_dataset', action='store_false', dest="k_fold_validation")
     root_parser.add_argument('--seed', type=int, default=0)
-    root_parser.add_argument('--precision', type=int, default=16, choices=[16, 32])
-    root_parser.add_argument('--distributed_strategy', choices=["none", "ddp_find_unused_parameters_false"], default="ddp_find_unused_parameters_false")
+    root_parser.add_argument('--precision', type=int, default=32, choices=[16, 32])
+    root_parser.add_argument('--distributed_strategy', choices=["none", "ddp_find_unused_parameters_false"],
+                             default="ddp_find_unused_parameters_false")
     root_parser.add_argument('--max_epochs', type=int, default=300, help="Maximum number of epochs to perform; the trainer will Exit after.")
+    root_parser.add_argument('--stocastic_weighted_averaging', action="store_true", dest="stochastic_weight_averaging", default="False")
+    root_parser.add_argument('--swa_epochs', type=int, default=10)
+    root_parser.add_argument('--swa_lr', type=float, default=1e-4)
+    root_parser.add_argument('--swa_epoch_start', type=float, default=0.8)
+
     root_parser.set_defaults(k_fold_validation=True)
 
     model_parser = TrainRTGENE.add_model_specific_args(root_parser)
@@ -231,19 +230,17 @@ if __name__ == "__main__":
     train_subs = []
     valid_subs = []
     test_subs = []
-    if hyperparams.dataset_type == "rt_gene":
+    if hyperparams.dataset_name == "rt_gene":
         if hyperparams.k_fold_validation:
-            train_subs.append([1, 2, 8, 10, 3, 4, 7, 9])
-            train_subs.append([1, 2, 8, 10, 5, 6, 11, 12, 13])
-            train_subs.append([3, 4, 7, 9, 5, 6, 11, 12, 13])
-            # validation set is always subjects 14, 15 and 16
-            valid_subs.append([0, 14, 15, 16])
-            valid_subs.append([0, 14, 15, 16])
-            valid_subs.append([0, 14, 15, 16])
-            # test subjects
-            test_subs.append([5, 6, 11, 12, 13])
-            test_subs.append([3, 4, 7, 9])
-            test_subs.append([1, 2, 8, 10])
+            train_subs.append([3, 4, 7, 9, 5, 6, 11, 12, 13, 14, 15, 16])
+            train_subs.append([1, 2, 8, 10, 5, 6, 11, 12, 13, 14, 15, 16])
+            train_subs.append([1, 2, 8, 10, 3, 4, 7, 9, 14, 15, 16])
+            valid_subs.append([1, 2, 8, 10])
+            valid_subs.append([3, 4, 7, 9])
+            valid_subs.append([5, 6, 11, 12, 13])
+            test_subs.append([0])
+            test_subs.append([0])
+            test_subs.append([0])
         else:
             train_subs.append([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
             valid_subs.append([0])  # Note that this is a hack and should not be used to get results for papers
@@ -264,19 +261,30 @@ if __name__ == "__main__":
             test_subs.append([keys[0]])
 
     for fold, (train_s, valid_s, test_s) in enumerate(zip(train_subs, valid_subs, test_subs)):
-        model = TrainRTGENE(hparams=hyperparams, train_subjects=train_s, validate_subjects=valid_s, test_subjects=test_s)
+        # create the dataset for this type
+        if hyperparams.dataset_type == "hdf5":
+            dataset_partial = partial(RTGENEH5Dataset, root_path=hyperparams.dataset_path)
+        elif hyperparams.dataset_type == "folder":
+            dataset_partial = partial(RTGENEFileDataset, root_path=hyperparams.dataset_path)
+        else:
+            raise ValueError(f"Dataset type {hyperparams.dataset_path} not suitable")
+
+        model = TrainRTGENE(hparams=hyperparams, train_subjects=train_s, validate_subjects=valid_s, test_subjects=test_s, dataset=dataset_partial)
 
         # can't save valid_loss or valid_angle_loss as now that's a composite loss mediated by a training related parameter
-        checkpoint_callback = ModelCheckpoint(monitor='valid_GazeAngleAccuracyMetric', mode='min', verbose=False, save_top_k=10, save_last=True,
-                                              filename="{epoch}-{valid_loss:.2f}-{valid_GazeAngleAccuracyMetric:.2f}")
-        learning_rate_callback = LearningRateMonitor()
+        callbacks = [ModelCheckpoint(monitor='valid_loss', mode='min', verbose=False, save_top_k=10, save_last=True,
+                                     filename="{epoch}-{valid_loss:.2f}-{valid_GazeAngleAccuracyMetric:.2f}"), LearningRateMonitor()]
+
+        if hyperparams.stochastic_weight_averaging:
+            swa = StochasticWeightAveraging(swa_epoch_start=0.8, swa_lrs=hyperparams.swa_lr, annealing_epochs=hyperparams.swa_epochs)
+            callbacks.append(swa)
 
         # start training
         trainer = Trainer(gpus=hyperparams.gpu,
                           precision=int(hyperparams.precision),
-                          callbacks=[checkpoint_callback, learning_rate_callback],
+                          callbacks=callbacks,
                           strategy=None if hyperparams.distributed_strategy == "none" else hyperparams.distributed_strategy,
                           log_every_n_steps=10,
                           max_epochs=hyperparams.max_epochs)
         trainer.fit(model)
-        # trainer.test()
+        trainer.test(ckpt_path="best")
